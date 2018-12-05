316525bf2b7cf5f6a5e4dd4e4f63743051747106
Lazaro franco <43150704+LazaroFranco@users.noreply.github.com>
2018-12-05 12:58:06 -0500

    Activated-Player-Movement

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
 let canvas = document.getElementById('gameCanvas');
 let context = canvas.getContext('2d');
 
+// This function will load the game and my character.
+window.onload = function() {
+  canvas;
+  context;
+
+  let framesPerSecond = 1000;
+  setInterval(function() {
+    loop();
+  }, 120/framesPerSecond);
+};
+
 // Modifying the canvas
 const drawGame = () => {
   context.fillStyle = 'black';
@@ -26,12 +37,61 @@ const obstacles = () => {
 function object() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
-    context.moveTo(25, 25 );
-    context.lineTo(125, 25;
-    context.lineTo(125, 75);
-    context.lineTo(25, 75);
+    context.moveTo(25 + moveX, 25 + moveY);
+    context.lineTo(125 + moveX, 25 + moveY);
+    context.lineTo(125 + moveX, 75 + moveY);
+    context.lineTo(25 + moveX, 75 + moveY);
     context.closePath();
 
     context.fillStyle = "blue";
     context.fill();
+};
+
+// This is listening to key pressed from keyboard. WILL MOVE YOUR CHARACTER
+var moveX = 0;
+var moveY = 0;
+
+window.addEventListener("keydown", keysPressed, false);
+window.addEventListener("keyup", keysReleased, false);
+
+let keys = [];
+
+function keysPressed(e) {
+    // store an entry for every key pressed
+    keys[e.keyCode] = true;
+
+    // left
+    if (keys[37]) {
+      moveX -= 10;
+    }
+
+    // right
+    if (keys[39]) {
+      moveX += 10;
+    }
+
+    // down
+    if (keys[38]) {
+      moveY -= 10;
+    }
+
+    // up
+    if (keys[40]) {
+      moveY += 10;
+    }
+
+    e.preventDefault();
+
+    object();
+};
+
+function keysReleased(e) {
+    // mark keys that were released
+    keys[e.keyCode] = false;
+};
+// This is just hard coding, for the window.onload function.
+const loop = () => {
+  drawGame();
+  object();
+  obstacles();
 };
